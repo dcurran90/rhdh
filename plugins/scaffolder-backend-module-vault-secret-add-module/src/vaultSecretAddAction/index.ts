78d30abff98ca4f5c6352b208090a184ee935602
Dan Curran <danielcurran90@gmail.com>
2025-09-10 18:08:51 -0500

    v 0.0.7 and should be running on remote envs

--- plugins/scaffolder-backend-module-vault-secret-add-module/src/vaultSecretAddAction/index.ts
+++ plugins/scaffolder-backend-module-vault-secret-add-module/src/vaultSecretAddAction/index.ts
@@ -26,27 +26,65 @@ export function createVaultSecretAction(config: Config) {
       const vaultPathArray = ctx.input.path.split("/")
       if (vaultPathArray.length != 2) throw new Error('path must be "<mount>/<secret>"');
 
-      const vaultKey = ctx.input.key
-      const vaultValue = ctx.input.value
-      const payload = {
-        data: { [vaultKey]: vaultValue },       // single k/v from your template input
-      };
-
-
       ctx.logger.info('Writing secret to Vault…');
       ctx.logger.debug(`Mount=${vaultPathArray[0]} Path=${vaultPathArray[1]}`);
 
-      ctx.logger.info("TESTLOG1")
-      ctx.logger.info(vaultKey)
-      ctx.logger.info(vaultValue)
-      ctx.logger.info(vaultUrl)
-
-      ctx.logger.info(`${vaultUrl}/v1/${vaultPathArray[0]}/data/${vaultPathArray[1]}`)
+      ctx.logger.info(`${vaultUrl}/v1/${vaultPathArray[0]}/${vaultPathArray[1]}`)
 
       if (!vaultToken) {
         throw new Error('Missing vault token configuration');
       }
-      const response = await fetch(`${vaultUrl}/v1/${vaultPathArray[0]}/data/${vaultPathArray[1]}`, {
+
+      // Check for secret location or create
+      const checkResponse = await fetch(`${vaultUrl}/v1/sys/mounts/${vaultPathArray[0]}`, {
+        method: 'GET',
+        headers: {
+          'X-Vault-Token': vaultToken,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (checkResponse.status!= 200) {
+        ctx.logger.info(`Could not find mount path ${vaultPathArray[0]}. Attempting to create...`);
+
+        const createMountPayload = {
+          type: 'kv',
+          options: {
+            version: '2'
+          }
+        };
+      
+        const createMountResponse = await fetch(`${vaultUrl}/v1/sys/mounts/${vaultPathArray[0]}`, {
+          method: 'POST',
+          headers: {
+            'X-Vault-Token': vaultToken,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(createMountPayload)
+        });
+
+        if (!createMountResponse.ok) {
+          const errorText = await createMountResponse.text()
+          throw new Error(`Failed to create mount: ${createMountResponse.status}`)
+        }
+
+        ctx.logger.info(`Created mount path ${vaultPathArray[0]}.`)
+
+      }
+
+      
+      const vaultKey = ctx.input.key
+      const vaultValue = ctx.input.value
+      // const payload = {
+      //   data: { [vaultKey]: vaultValue },       // single k/v from your template input
+      // };
+      const payload = {
+        [vaultKey]: vaultValue,       // single k/v from your template input
+      };
+
+
+      // Post new secret
+      const response = await fetch(`${vaultUrl}/v1/${vaultPathArray[0]}/${vaultPathArray[1]}`, {
         method: 'POST',
         headers: {
           'X-Vault-Token': vaultToken,
@@ -56,7 +94,6 @@ export function createVaultSecretAction(config: Config) {
       });
 
       
-      ctx.logger.info("TESTLOG2")
       ctx.logger.info(`Vault response status: ${response.status}`);
       ctx.logger.info(`Vault response:}`);
       ctx.logger.info(JSON.stringify(response))
